e9874d66d799ab1ba3a22afb149badfedc37d086
alexeysurmach <flashhome1985@gmail.com>
2019-09-27 14:49:43 +0300

    basic сss and component breakdown

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src='https://bower.io/img/bower-logo.png'/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <a> <img src='https://cdn.sandals.com/beaches/v12/images/general/destinations/home/beach.jpg'/> </a>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Profile;
