451907de3db3e7a7fa7dbc431088e0f30d6bca5c
alexeysurmach <flashhome1985@gmail.com>
2019-09-29 03:16:25 +0300

    update css & component breakdown

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,26 +1,16 @@
 import React from 'react';
-
+import s from './Profile.module.css';
+import MyPosts from "./Myposts/MyPosts";
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={s.content}>;
             <div>
                 <a> <img src='https://cdn.sandals.com/beaches/v12/images/general/destinations/home/beach.jpg'/> </a>
             </div>
             <div>
                 ava + description
             </div>
-            <div>
-                My posts
-                <div>
-                    New post
-                </div>
-                <div>
-                    post 1
-                </div>
-                <div>
-                    post 2
-                </div>
-            </div>
+            <MyPosts/>
         </div>
 
     )
