15cadb08257c514dc503e9516f1d2c18a8800dab
alexeysurmach <flashhome1985@gmail.com>
2019-10-09 13:42:44 +0300

    refactoring Dilogs&Posts

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -12,20 +12,28 @@ const Message = (props) => {
 }
 
 const Dialogs = (props) => {
+    let dialogs = [
+        {id: 1, name: 'Nastya'},
+        {id: 2, name: 'Roma'},
+        {id: 3, name: 'Leha'},
+        {id: 4, name: 'Max'}
+    ];
+    let messages = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'Cowabanga'},
+        {id: 3, message: 'Hellow'},
+    ];
+
+    let dialogsDialogElements = dialogs.map(d => <DilogItem name={d.name} id={d.id}/>);
+    let messagesElements = messages.map(m => <Message message={m.message}/>);
+
     return (
         <div className={s.dialogs}>
             <div className={s.dilogsItems}>
-                <DilogItem name='Nastya' id='1'/>
-                <DilogItem name='Roma' id='2'/>
-                <DilogItem name='Leha' id='3'/>
-                <DilogItem name='Max' id='4'/>
-
+                {dialogsDialogElements}
             </div>
             <div className={s.messages}>
-                <Message message='Hi'/>
-                <Message message='Cowabanga'/>
-                <Message message='Hellow'/>
-
+                {messagesElements}
             </div>
         </div>
     )
--- src/components/Profile/Myposts/MyPosts.jsx
+++ src/components/Profile/Myposts/MyPosts.jsx
@@ -3,16 +3,23 @@ import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+    let posts = [
+        {id: 1, message: 'Hey', countLikes: 7},
+        {id: 2, message: 'Cowabanga', countLikes: 21}
+    ];
+    let postElements = posts.map( p => <Post message={p.message} countLikes={p.countLikes}/>)
     return (
         <div className={s.postBlock}>
             <h3> My posts </h3>
             <div>
-                <div> <textarea> </textarea> </div>
-                <div> <button>Ad post</button> </div>
+                <div><textarea> </textarea></div>
+                <div>
+                    <button>Ad post</button>
+                </div>
             </div>
             <div className={s.posts}>
-                <Post message='Hey' countLikes = '7'/>
-                <Post message='Cowabanga' countLikes = '21'/>
+                {postElements}
+                {/*<Post message={postData[0].message} countLikes={postData[0].countLikes}/>*/}
             </div>
         </div>
     )
